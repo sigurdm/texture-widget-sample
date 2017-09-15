e965110d98f85a740baf25e84fbe3c3aecd127d4
Sigurd Meldgaard <sigurdm@google.com>
2017-09-15 10:42:03 +0200

    Hole through

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(
-    new DecoratedBox(
+MethodChannel textureChannel = new MethodChannel("flutter/textures");
+
+class TextureHolder extends StatefulWidget {
+  createState() => new TextureHolderState();
+}
+
+class TextureHolderState extends State<TextureHolder>
+    with TickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        new AnimationController(vsync: this, duration: new Duration(days: 2));
+    controller.animateTo(1.0);
+  }
+
+  AnimationController controller;
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new DecoratedBox(
       decoration: new BoxDecoration(color: Colors.blue),
       child: new Center(
         child: new SizedBox(
@@ -10,10 +35,17 @@ void main() {
           height: 100.0,
           child: new DecoratedBox(
             decoration: new BoxDecoration(border: new Border.all()),
-            child: new Texture(textureId: 2),
+            child: new AnimatedTexture(controller),
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+void main() {
+  runApp(new MaterialApp(
+      home: new Scaffold(
+    body: new TextureHolder(),
+  )));
 }
