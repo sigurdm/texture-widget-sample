7fedd85e764bab4029ef43304a627e8eb1ed605e
Sigurd Meldgaard <sigurdm@google.com>
2017-09-18 09:42:27 +0200

    Use cards

--- lib/main.dart
+++ lib/main.dart
@@ -27,25 +27,90 @@ class TextureHolderState extends State<TextureHolder>
 
   @override
   Widget build(BuildContext context) {
-    return new DecoratedBox(
-      decoration: new BoxDecoration(color: Colors.blue),
-      child: new Center(
-        child: new SizedBox(
-          width: 100.0,
-          height: 100.0,
-          child: new DecoratedBox(
-            decoration: new BoxDecoration(border: new Border.all()),
-            child: new AnimatedTexture(controller),
-          ),
-        ),
-      ),
+    return new SizedBox(
+      width: 100.0,
+      height: 100.0,
+      child: new AnimatedTexture(controller),
     );
   }
+}
+
+Widget buildCard(String title) {
+  return new Card(
+    child: new Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        new ListTile(
+          leading: const Icon(Icons.airline_seat_flat_angled),
+          title: new Text(title),
+          //      subtitle: const Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+        ),
+        new ButtonTheme.bar(
+          // make buttons use the appropriate styles for cards
+          child: new ButtonBar(
+            children: <Widget>[
+              new FlatButton(
+                child: const Text('BUY TICKETS'),
+                onPressed: () {/* ... */},
+              ),
+              new FlatButton(
+                child: const Text('LISTEN'),
+                onPressed: () {/* ... */},
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
 }
 
 void main() {
   runApp(new MaterialApp(
       home: new Scaffold(
-    body: new TextureHolder(),
+    body: new ListView(
+      children: [
+        buildCard("Airline a"),
+        buildCard("Airline b"),
+        buildCard("Airline c"),
+        buildCard("Airline d"),
+        buildCard("Airline e"),
+        buildCard("Airline f"),
+        buildCard("Airline g"),
+        new Card(
+            child: new Column(children: [
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              new SizedBox(
+                  width: 280.0, height: 210.0, child: new TextureHolder()),
+              new Text(
+                "video video!",
+              ),
+            ],
+          ),
+          new ButtonTheme.bar(
+            // make buttons use the appropriate styles for cards
+            child: new ButtonBar(
+              children: <Widget>[
+                new FlatButton(
+                  child: const Text('BUY TICKETS'),
+                  onPressed: () {/* ... */},
+                ),
+                new FlatButton(
+                  child: const Text('LISTEN'),
+                  onPressed: () {/* ... */},
+                ),
+              ],
+            ),
+          ),
+        ])),
+        buildCard("Airline h"),
+        buildCard("Airline i"),
+        buildCard("Airline j"),
+        buildCard("Airline k"),
+        buildCard("Airline l"),
+      ],
+    ),
   )));
 }
